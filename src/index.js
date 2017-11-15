efca3912ad9f322056684c1e33114f63a4c4b648
johncurcio <joao8177@gmail.com>
2017-11-15 19:43:28 -0200

    Improve templating

--- src/index.js
+++ src/index.js
@@ -7,31 +7,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var stories;
-fs.readFile('stories.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  stories = JSON.parse(data);
-});
-
-app.get('/', (req, res) => {
-  res.render('index', 
-  		{ stories: stories }
-  	)
-});
-
-app.get('/story/:id', (req, res) => {
-  const story = stories.filter((stry) => {
-    return stry.id == req.params.id
-  })[0]
-
-  res.render('story', {
-    author: story.author,
-    title: story.title,
-    genre: story.genre,
-	createdat: story.createdat,
-	fragments: story.fragments
-  })
-});
+var mainRoutes = require('./routes/main')
+app.use(mainRoutes)
 
 app.listen(8080);
 
---
+++ src/routes/main.js
@@ -0,0 +1,30 @@
+var router   = require('express').Router()
+var fs       = require('fs');
+
+var stories;
+fs.readFile('stories.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  stories = JSON.parse(data);
+});
+
+router.get('/', (req, res) => {
+  res.render('index', 
+        { stories: stories }
+    )
+});
+
+router.get('/story/:id', (req, res) => {
+  const story = stories.filter((stry) => {
+    return stry.id == req.params.id
+  })[0]
+
+  res.render('story', {
+    author: story.author,
+    title: story.title,
+    genre: story.genre,
+    createdat: story.createdat,
+    fragments: story.fragments
+  })
+});
+
+module.exports = router
